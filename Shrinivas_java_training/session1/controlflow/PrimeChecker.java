b2cc5b3d6ac602378a750667f43f4e1ee5efb844
Shinu87 <shrinivasmore51@gmail.com>
2026-04-08 16:28:30 +0530

    [Session1-ControlFlow] Add PrimeChecker, LargestNumberFinder programs

---
+++ Shrinivas_java_training/session1/controlflow/PrimeChecker.java
@@ -0,0 +1,38 @@
+package Shrinivas_java_training.session1.controlflow;
+
+import java.util.Scanner;
+
+public class PrimeChecker {
+
+    // Method to check if a number is prime
+    public static boolean checkPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        // Check divisibility from 2 up to sqrt(num) for efficiency
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number to check: ");
+        int num = scanner.nextInt();
+
+        boolean isPrime = checkPrime(num);
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+}
