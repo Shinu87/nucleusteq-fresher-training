c1a29e3d41d47627def4bae63139724a7d04377a
Shinu87 <shrinivasmore51@gmail.com>
2026-04-18 11:49:24 +0530

    Create Todo entity with JPA annotations

---
+++ Shrinivas_java_training/java/session4/todo-app/src/main/java/com/shinu/todo_app/entity/Todo.java
@@ -0,0 +1,27 @@
+package com.shinu.todo_app.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+    private String description;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+
+}
