ce2e0d662564f713ba261aee2e034490cd26eace
Shinu87 <shrinivasmore51@gmail.com>
2026-04-21 11:44:06 +0530

    setup: Initialize backend with basic Spring Boot structure

---
+++ Capstone_Interview_Tracking_App/backend/src/test/java/com/capstone/interviewtracker/InterviewtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.interviewtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
