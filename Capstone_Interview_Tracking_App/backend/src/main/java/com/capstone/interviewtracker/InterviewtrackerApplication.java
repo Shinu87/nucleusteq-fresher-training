256e2626441198a7d6f8842d65f5d8ca0c022f6c
Shinu87 <shrinivasmore51@gmail.com>
2026-04-23 09:25:44 +0530

    Setup PostgreSQL config

--- Capstone_Interview_Tracking_App/backend/src/main/java/com/capstone/interviewtracker/InterviewtrackerApplication.java
+++ Capstone_Interview_Tracking_App/backend/src/main/java/com/capstone/interviewtracker/InterviewtrackerApplication.java
@@ -1,5 +1,6 @@
 package com.capstone.interviewtracker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InterviewtrackerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(InterviewtrackerApplication.class, args);
 	}
 
