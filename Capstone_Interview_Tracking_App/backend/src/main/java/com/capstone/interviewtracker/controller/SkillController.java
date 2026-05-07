edf01198c26e3b698322448bf9649b7e410f7057
Shinu87 <shrinivasmore51@gmail.com>
2026-05-07 08:33:51 +0530

    (backend)improved skill and resume controllers with secured access

--- Capstone_Interview_Tracking_App/backend/src/main/java/com/capstone/interviewtracker/controller/SkillController.java
+++ Capstone_Interview_Tracking_App/backend/src/main/java/com/capstone/interviewtracker/controller/SkillController.java
@@ -3,6 +3,7 @@ package com.capstone.interviewtracker.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,7 @@ public class SkillController {
      *
      * @return list of skills
      */
+    @PreAuthorize("hasRole('HR')")
     @GetMapping
     public ResponseEntity<List<SkillResponseDTO>> getAllSkills() {
         return ResponseEntity.ok(skillService.getAllSkills());
