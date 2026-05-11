f2be5ea9c5a5be99277d4d5f664ca6d971887ff0
Shinu87 <shrinivasmore51@gmail.com>
2026-05-11 09:36:32 +0530

    Added SMTP configuration using dotenv setup

--- Capstone_Interview_Tracking_App/backend/src/main/java/com/capstone/interviewtracker/InterviewtrackerApplication.java
+++ Capstone_Interview_Tracking_App/backend/src/main/java/com/capstone/interviewtracker/InterviewtrackerApplication.java
@@ -8,10 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InterviewtrackerApplication {
 
 	public static void main(String[] args) {
+
 		Dotenv dotenv = Dotenv.load();
 
+		// Database
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		// SMTP
+		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+
 		SpringApplication.run(InterviewtrackerApplication.class, args);
 	}
 
