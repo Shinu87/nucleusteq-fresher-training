3d3931cc03c8113c3d6b4109cd2bec62c216fa5a
Shinu87 <shrinivasmore51@gmail.com>
2026-06-21 16:18:38 +0530

    packaging: modules, package structure and module vs package examples

---
+++ Shrinivas_python_training/python_advance/Assignment_1/packaging/employee_package/employee_details.py
@@ -0,0 +1,11 @@
+# Module for employee information.
+
+def get_employee_details() -> dict:
+    """
+    Display employee details.
+    """
+    return {
+        "id": 101,
+        "name": "Rahul Sharma",
+        "department": "Engineering"
+    }
---
+++ Shrinivas_python_training/python_advance/Assignment_1/packaging/employee_package/employee_salary.py
@@ -0,0 +1,7 @@
+# Module for salary information.
+
+def calculate_annual_salary(monthly_salary: int) -> int:
+    """
+    Find annual salary using monthly salary.
+    """
+    return monthly_salary * 12
